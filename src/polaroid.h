4ea6808ae41f95d32395d25ac160b84718c1de75
Kael Paraguassu <kaelparaguassu@hotmail.com>
2023-06-28 10:27:40 -0300

    implementacao da classe polaroid

---
+++ src/polaroid.h
@@ -0,0 +1,14 @@
+#ifndef __POLAROID_H__
+#define __POLAROID_H__
+
+using namespace std;
+
+/**
+ * Classe que representa a imagem com o estilo polaroid.
+ */
+
+class Polaroid {
+
+};
+
+#endif  // POLAROID_H
