557e1c7fb71c4ff88da1e25dbdb85d134a7b6d74
Kael Paraguassu <kaelparaguassu@hotmail.com>
2023-07-12 22:37:11 -0300

    adicao de comentarios na classe pixel

--- src/pixel.h
+++ src/pixel.h
@@ -6,8 +6,8 @@ using namespace std;
 /**
  * Classe que representa um pixel.
  */
-
 class Pixel {
+    // valores rgb do pixel.
     unsigned char red;
     unsigned char green;
     unsigned char blue;
@@ -18,10 +18,13 @@ public:
 
     Pixel(unsigned char r, unsigned char g, unsigned char b);
 
+    /** Consulta o valor do vermelho no pixel. */
     unsigned char getRed();
 
+    /** Consulta o valor do verde no pixel. */
     unsigned char getGreen();
 
+    /** Consulta o valor do azul no pixel. */
     unsigned char getBlue();
 
 };
